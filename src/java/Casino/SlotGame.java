45800221cafe8a09fc829bc8e03560214d75accd
msdiego7 <diegomorrosilva@gmail.com>
2022-06-04 18:41:56 +0200

    Casino Backend

--- src/java/Casino/SlotGame.java
+++ src/java/Casino/SlotGame.java
@@ -1,13 +1,11 @@
 
 package Casino;
 
-import java.lang.Math;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
